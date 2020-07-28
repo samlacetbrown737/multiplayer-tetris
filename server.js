7b87df380232ec52488ca7260d365dc35fb83e0c
x9du <33583761+x9du@users.noreply.github.com>
2020-07-28 13:18:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+var start1;
+var start2;
+
 var express = require('express');
 var app = express();
 var server = app.listen(process.env.PORT || 56151, listen);
-	// var start1, start2;
 
 function listen() {
   var host = server.address().address;
@@ -12,69 +14,38 @@ function listen() {
 app.use(express.static('public'));
 
 var io = require('socket.io')(server);
-io.sockets.on('connection',
-  function (socket) {
-  
-	console.log("We have a new client: " + socket.id);
+io.sockets.on('connection', function (socket) {
+  console.log("We have a new client: " + socket.id);
 
-	socket.on('move',
-		function(data) {
-			console.log("Key " + data.keyPressed + " from player " + data.user);
-			socket.broadcast.emit('move', data);
-		}
-	);
+  socket.on('move', function(data) {
+    console.log("Key " + data.keyPressed + " from player " + data.user);
+    socket.broadcast.emit('move', data);
+  });
 
-	socket.on('newPiece',
-	  function(data) {
-		console.log("Piece " + data.id);
-		socket.broadcast.emit('newPiece', data);
-	  }
-	);
+  socket.on('newPiece', function(data) {
+    console.log("Piece " + data.id);
+    socket.broadcast.emit('newPiece', data);
+  });
 
-	socket.on('pause',
-	  function(data) {
-		console.log("Received: 'pause' " + data.pause);
-		socket.broadcast.emit('pause', data);
-	  }
-	);
+  socket.on('pause', function(data) {
+    console.log("Received: 'pause' " + data.pause);
+    socket.broadcast.emit('pause', data);
+  });
 
-	// socket.on('start',
-	//   function(data) {
-	// 	console.log("Received: 'start' " + data.player);
-	// 	if(data.player == "one") {
-	// 	  start1 = true;
-	// 	} else if (data.player == "two") {
-	// 	  start2 = true;
-	// 	}
-	// 	console.log(start1 + " " + start2);
-	// 	if(start1 && start2) {
- //        	socket.broadcast.emit('start', data);
- //    	}
-	//   }
-	// );
+  socket.on('start', function(data) {
+    console.log("Received: 'start' " + data.user);
+    if(data.user == "one") {
+      start1 = data.start;
+    } else {
+      start2 = data.start;
+    }
 
-	var start1 = false;
-	var start2 = false;
-	 socket.on('start',
-      function(data) {
-        console.log("Received: 'start' " + data.user);
-        // if(data.user == "one") {
-        //   start1 = data.start;
-        // } else {
-        //   start2 = data.start;
-        // }
+    if(start1 && start2) {
+      io.sockets.emit('start', true);
+    }
+  });
 
-        // if(start1 && start2) {
-        //   io.sockets.emit('startGame', true);
-        // }
-
-        io.sockets.emit('startGame', data);
-      }
-     );
-	
-	socket.on('disconnect', function() {
-	  console.log("Client has disconnected");
-	});
-  }
-);
-
+  socket.on('disconnect', function() {
+    console.log("Client has disconnected");
+  });
+});
